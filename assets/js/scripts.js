a43c78c86ba518c3d3c89800e129c20b885e9ffd
Marcos Azevedo <marcosazevedo2112@gmail.com>
2023-02-17 10:45:02 -0300

    versão desktop pronta | proximo: revisão + fotos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,8 @@
 const swiper = new Swiper('.swiper', {
     speed: 1000,
     spaceBetween: 80,
-    autoplay:{
+    grabCursor: true,
+    autoplay: {
         delay: 2000,
         disableOnInteraction: false,
         waitForTransition: true,
@@ -9,4 +10,18 @@ const swiper = new Swiper('.swiper', {
     parallax: true,
     slidesPerView: 'auto',
     centeredSlides: true,
-  });
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+            loop: true,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+            loop: true
+        }
+    }
+});
